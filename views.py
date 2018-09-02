5f560a17359eb785d1f7013ee203bb21ce881409
winstonpan <41726289+winstonpan@users.noreply.github.com>
2018-09-02 21:14:16 +1000

    Add files via upload

---
+++ views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+	#return HttpResponse("<h1>Hello World</h1>")
+	print(args, kwargs)
+	print(request.user)
+ 
+	return render(request, 'home.html', {})
