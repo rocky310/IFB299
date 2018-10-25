0e7f1d581026f619264a4a491bc46fcb8cbf6853
winstonpan <41726289+winstonpan@users.noreply.github.com>
2018-10-26 08:48:34 +1000

    Add files via upload

---
+++ Winston/databases/accounts_mitsubishi.sql
@@ -0,0 +1,5 @@
+INSERT INTO `accounts_mitsubishi` VALUES (1, 'Mitsubishi Lancer', 'Tdi 4x4', '1996', '2.5L', 'DIESEL TURBO', '88.5L', '83Kw', '4 Seats', '4D WAGON', 'Automatic', 'images/mitsubishi-lancer.png');
+INSERT INTO `accounts_mitsubishi` VALUES (2, 'Mitsubishi Sportero', 'XEi 4x4', '2000', '1.8L', 'MULTI POINT F/INJ', '59L', '84Kw', '4 Seats', '3D HARDBACK', 'Automatic', 'images/mitsubishi-sportero.png');
+INSERT INTO `accounts_mitsubishi` VALUES (3, 'Mitsubishi Eclipse', 'S15 SX SPEC S', '2001', '2.0L', 'TURBO MPFI', '65L', '147Kw', '7 Seats', '4D HATCHBACK', '4D HATCHBACK', 'images/ZEAUCGAR999.jpg');
+INSERT INTO `accounts_mitsubishi` VALUES (4, 'Mitsubishi Pajero Sport GLX', 'Mi16', '2000', '2.0L', 'MULTI POINT F/INJ', '60L', '100Kw', '5 Seats', '5D HATCHBACK', 'Automatic/Standard', 'images/ZEAUSFAR999.jpg');
+INSERT INTO `accounts_mitsubishi` VALUES (5, 'Mitsubishi MIRAGE HATCH', 'PROTEGE', '2006', '1.6L', 'MULTI POINT F/INJ', '55L', '78Kw', '4 Seats', '4D SEDAN', 'Automatic', 'images/Mirage18-Hero-001_455VVJn.png');
